233875a374c00c4a7e1539e127e4dd26bfa5b447
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-03-31 06:39:30 +0300

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        let res = [];
+        let deque = [];
+        
+        for (let r = 0; r < nums.length; r++) {
+            while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[r]) {
+                deque.pop();
+            };
+            deque.push(r);
+
+            if (deque[0] < r - k + 1) {
+                deque.shift();
+            }
+
+            if (r >= k - 1) {
+                res.push(nums[deque[0]]);
+            }
+        }
+        return res;
+    }
+}
