03719361caeb11120114751b04f61b6ed434dcfb
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-03-31 06:24:39 +0300

    Add: longest-substring-without-duplicates - submission-20

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-20.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let charSet = new Set();
+        let res = 0;
+
+        let l = 0;
+        for (let r = 0; r < s.length; r++) {
+            while (charSet.has(s[r])) {
+                charSet.delete(s[l]);
+                l++;
+            }
+
+            charSet.add(s[r]);
+
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
