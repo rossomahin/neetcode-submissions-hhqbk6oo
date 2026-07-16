5c224cf986846e59e952cf9ea0601780c21c6302
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-07-16 05:10:31 +0300

    Add: valid-sudoku - submission-20

---
+++ Data Structures & Algorithms/valid-sudoku/submission-20.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        for (let r = 0; r < 9; r++) {
+            const seen = new Set();
+            for (let c = 0; c < 9; c++) {
+                if (board[r][c] === '.') continue;
+                if (seen.has(board[r][c])) return false;
+                seen.add(board[r][c]);
+            }
+        }
+        for (let c = 0; c < 9; c++) {
+            const seen = new Set();
+            for (let r = 0; r < 9; r++) {
+                if (board[r][c] === '.') continue;
+                if (seen.has(board[r][c])) return false;
+                seen.add(board[r][c]);
+            }
+        }
+        for (let square = 0; square < 9; square++) {
+            const seen = new Set();
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    let r = Math.floor(square / 3) * 3 + i;
+                    let c = (square % 3) * 3 + j;
+
+                    if (board[r][c] === '.') continue; 
+                    if (seen.has(board[r][c])) return false;
+
+                    seen.add(board[r][c]) 
+                }
+            }
+        }
+
+        return true;
+    }
+}
