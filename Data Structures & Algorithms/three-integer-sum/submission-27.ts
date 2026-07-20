0b6904b8c4069489eb9e7e599840b0c6e90a9180
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-07-20 19:46:18 +0300

    Add: three-integer-sum - submission-27

---
+++ Data Structures & Algorithms/three-integer-sum/submission-27.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        nums.sort((a, b) => a - b)
+        const output = [];
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] > 0) break; // разрушаем итерацию, так число 0 не получим из положительных чисел
+            if (i > 0 && nums[i] === nums[i - 1]) continue // пропускаем итерацию, чтобы не было дубликатов
+
+            let left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                const sum = nums[i] + nums[left] + nums[right];
+
+                if (sum === 0) {
+                    output.push([nums[i], nums[left], nums[right]]);
+
+                    while (left < right && nums[left] === nums[left + 1]) left++; // пропускаем дубликаты
+                    while (left < right && nums[right] === nums[right - 1]) right--; // пропускаем дубликаты   
+
+                    left++;
+                    right--;               
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return output;
+    }
+}
