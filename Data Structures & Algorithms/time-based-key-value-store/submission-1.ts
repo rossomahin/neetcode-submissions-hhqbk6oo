9eaeac04f5543a27b377b061889362dbca2ca29e
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-07-26 04:32:36 +0300

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.ts
@@ -0,0 +1,46 @@
+class TimeMap {
+    keyStore: Map<string, [string, number][]>;
+    
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key: string, value: string, timestamp: number): void {
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, [])
+        }
+
+        this.keyStore.get(key).push([value, timestamp]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key: string, timestamp: number): string {
+        let values = this.keyStore.get(key);
+        if (!values) return "";
+
+        let left = 0, right = values.length - 1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (values[mid][1] > timestamp) {
+                right = mid - 1;
+            } else if (values[mid][1] < timestamp) {
+                left = mid + 1;
+            } else if (values[mid][1] === timestamp) {
+                return values[mid][0];
+            }
+        }
+        return right >= 0 ? values[right][0] : "";
+    }
+}
