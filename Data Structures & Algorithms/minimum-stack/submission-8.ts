b7f8fc8fb5b68979ef23577c89caf31b6e2ea517
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-08-10 00:50:01 +0300

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.ts
@@ -0,0 +1,44 @@
+class MinStack {
+    stack: number[]
+    minStack: number[]
+    constructor() { 
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack[this.stack.length] = val;
+        const currentVal: number = 
+                this.minStack.length === 0 ?
+                    val : Math.min(this.minStack[this.minStack.length - 1], val);
+        this.minStack[this.minStack.length] = currentVal;
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.stack.length > 0) {
+            this.stack.length -= 1;
+            this.minStack.length -= 1;
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
