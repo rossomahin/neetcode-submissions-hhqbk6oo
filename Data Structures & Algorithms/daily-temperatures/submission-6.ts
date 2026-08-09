a86b21918f84ff466a912e9d3f76b89af05c2e42
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-08-10 00:59:36 +0300

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        const stack: number[][] = [];
+        const res: number[] = new Array(temperatures.length).fill(0);
+        for (let i = 0; i < temperatures.length; i++) {
+            while (
+                stack.length && stack[stack.length - 1][1] < temperatures[i]
+            ) {
+                const [prevIdx] = stack.pop()!;
+                res[prevIdx] = i - prevIdx;
+            }
+            stack.push([i, temperatures[i]])
+        }
+
+        return res;
+    }
+}
