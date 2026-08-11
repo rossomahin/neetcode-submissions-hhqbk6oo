d8823ce5d93d4827b1483a516ad803731b4b2154
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-08-11 16:21:05 +0300

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target: number, position: number[], speed: number[]): number {
+        const stack: number [] = [];
+        const cars: number[][] = position.map((pos, i) => [pos, speed[i]]);
+        cars.sort((a, b) => b[0] - a[0]);
+
+        for (const [pos, speed] of cars) {
+            const time = (target - pos) / speed;
+
+            if (stack.length === 0 || time > stack[stack.length - 1]) {
+                stack.push(time);
+            }
+        }
+
+        return stack.length;
+    }
+}
