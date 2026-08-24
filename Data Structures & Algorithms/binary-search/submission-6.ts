01936a7feca9d61da0d575b01379db317d6358f7
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-08-24 18:31:15 +0300

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left: number = 0, right: number = nums.length;
+
+        while (left <= right) {
+            const mid: number = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                return mid
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
