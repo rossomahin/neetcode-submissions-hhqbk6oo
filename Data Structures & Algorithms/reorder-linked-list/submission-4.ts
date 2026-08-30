ac96c930ee571a02c9485e0269961dc9848eb648
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-08-30 21:41:04 +0300

    Add: reorder-linked-list - submission-4

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-4.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head: ListNode | null): void {
+        let slowPointer = head, fastPointer = head;
+        while (fastPointer !== null && fastPointer.next !== null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        let prev = null, current = slowPointer.next, next = null;
+        while (current !== null) {
+            next = current.next;
+            current.next = prev;
+
+            prev = current;
+            current = next;
+        }
+        slowPointer.next = null;
+
+        let first = head, second = prev;
+        while (second !== null) {
+            let current1 = first.next;
+            let current2 = second.next;
+
+            first.next = second;
+            second.next = current1;
+
+            first = current1;
+            second = current2;
+        }
+    }
+}
