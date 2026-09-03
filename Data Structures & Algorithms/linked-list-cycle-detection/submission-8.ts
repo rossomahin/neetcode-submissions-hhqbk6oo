c7360b29cee9a08249034505701f2c15fd9cbff8
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-09-03 09:45:00 +0300

    Add: linked-list-cycle-detection - submission-8

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-8.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        if (head === null) return false;
+
+        let slowPointer = head, fastPointer = head;
+
+        while (fastPointer !== null && fastPointer.next !== null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if (slowPointer === fastPointer) return true;
+        }
+
+        return false;
+    }
+}
