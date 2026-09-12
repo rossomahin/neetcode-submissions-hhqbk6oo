3729bd1eb486dd82e7273af63d22fe39374fc4cb
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-09-12 15:16:39 +0300

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root: TreeNode | null): boolean {
+        if (root === null) return true;
+        return this.depth(root) !== -1;
+    }
+    private depth(root: TreeNode | null) : number {
+        if (root === null) return 0;
+
+        let left = this.depth(root.left);        
+        let right = this.depth(root.right);
+
+        if (left === -1 || right === -1) {
+            return -1;
+        } else if (Math.abs(left - right) > 1) {
+            return -1
+        }
+        return 1 + Math.max(left, right);
+    }
+}
