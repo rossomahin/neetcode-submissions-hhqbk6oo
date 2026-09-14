7909f23b5e89110cd929d84ba0f2d734f83f7e61
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-09-14 05:28:45 +0300

    Add: validate-parentheses - submission-23

---
+++ Data Structures & Algorithms/validate-parentheses/submission-23.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const closeToOpen = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+        const stack = [];
+
+        for (let char of s) {
+            if (
+                stack.length && closeToOpen[char] === stack[stack.length - 1]
+            ) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        }
+        return stack.length === 0;
+    }
+}
