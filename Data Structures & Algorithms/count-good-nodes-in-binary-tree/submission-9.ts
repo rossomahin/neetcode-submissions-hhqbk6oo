5ef78b3f7cdff38078a901ebb113b71f8dd1cd01
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-09-17 04:32:21 +0300

    Add: count-good-nodes-in-binary-tree - submission-9

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-9.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root: TreeNode | null): number {
+        if (root === null) return 0;
+
+        return this._maxDepth(root);
+    }
+
+    private _maxDepth(root: TreeNode | null, max = -Infinity) {
+        if (root === null) return 0;
+
+        const goodNodes = root.val >= max ? 1 : 0;
+        max = Math.max(max, root.val);
+
+        return goodNodes + this._maxDepth(root.left, max) + this._maxDepth(root.right, max);
+    }
+}
