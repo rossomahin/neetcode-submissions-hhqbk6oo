ae4f995db8c7af9468aea6c983d0b513f9e6119d
rossomahin <129364322+rossomahin@users.noreply.github.com>
2026-09-21 20:38:08 +0300

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n: number): string[] {
+        const res = [];
+        function dfs(current, open, close) {
+            if (open === n && close === n) res.push(current);
+
+            if (open < n) {
+                dfs(current + '(', open + 1, close);
+            } 
+            if (close < open) {
+                dfs(current + ')', open, close + 1);
+            }
+        }
+        dfs([], 0, 0);
+        return res;
+    }
+}
